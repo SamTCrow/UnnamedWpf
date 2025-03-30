e2191e46e108ba8b074bf9b8f23f762397e37f3a
SamTCrow <162122571+SamTCrow@users.noreply.github.com>
2025-03-30 23:08:43 +0200

    Ristrutturazione delle classi e gestione delle risorse
    Le modifiche apportate includono l'introduzione delle classi `GameItem`, `ItemQuantity`, `Quest`, e `Weapon`, migliorando la gestione degli oggetti di gioco e delle quest. La classe `Player` è stata aggiornata per estendere `ObservableObject`, ottimizzando la gestione delle notifiche di cambiamento delle proprietà. Inoltre, sono state aggiunte le classi `ItemFactory`, `QuestFactory` e `WorldFactory` per una migliore organizzazione del codice. Infine, sono state apportate modifiche al file `Engine.csproj` per gestire le risorse delle immagini, rimuovendo quelle obsolete e aggiungendo nuove immagini necessarie.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,15 +1,6 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 
-using System.Text;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using Engine.ViewModels;
 
 namespace UnnamedWpf;
@@ -28,8 +19,23 @@ public partial class MainWindow : Window
         DataContext = _gameSession;
     }
 
-    private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+    private void OnClick_MoveNorth(object sender, RoutedEventArgs e)
     {
-        _gameSession.CurrentPlayer.AddXp(10);
+        _gameSession.Move(Direction.North);
+    }
+
+    private void OnClick_MoveSouth(object sender, RoutedEventArgs e)
+    {
+        _gameSession.Move(Direction.South);
+    }
+
+    private void OnClick_MoveEast(object sender, RoutedEventArgs e)
+    {
+        _gameSession.Move(Direction.East);
+    }
+
+    private void OnClick_MoveWest(object sender, RoutedEventArgs e)
+    {
+        _gameSession.Move(Direction.West);
     }
 }
