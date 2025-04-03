57638e16553298f71506135aa1ad49c7ab9b7cd3
SamTCrow <162122571+SamTCrow@users.noreply.github.com>
2025-04-03 21:53:36 +0200

    Aggiunte nuove funzionalità e miglioramenti UI
    - Implementata la funzione `ItemName` in `ItemFactory` per ottenere il nome di un oggetto tramite ID.
    - Modificata la logica in `RecipeFactory` per utilizzare `AddOutputItem` invece di `AddIngredient`.
    - Aggiunte le funzioni `RemoveItems` e `HasAllTheseItems` nella classe `LivingEntity`.
    - Rimosso il metodo `HasAllTheseItems` dalla classe `Player`, ora utilizza la versione della classe base.
    - Aggiornato `_currentPlayer` in `GameSession` per supportare valori nullabili e gestire correttamente gli eventi.
    - Modifiche significative all'interfaccia utente in `MainWindow.xaml`, inclusa l'aggiunta di un pulsante "Craft".
    - Implementata la gestione degli input utente in `MainWindow.xaml.cs` per migliorare l'interazione.
    - Aggiunti file di configurazione e aggiornato `.gitignore` per escludere file specifici dell'ambiente di sviluppo.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,8 +1,11 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Documents;
+using System.Windows.Input;
 using Engine.EventArgs;
+using Engine.Models;
 using Engine.ViewModels;
 
 namespace UnnamedWpf;
@@ -10,13 +13,15 @@ namespace UnnamedWpf;
 /// <summary>
 /// Interaction logic for MainWindow.xaml
 /// </summary>
-public partial class MainWindow : Window
+public partial class MainWindow
 {
     private readonly GameSession _gameSession = new();
+    private readonly Dictionary<Key, Action> _userInputActions = [];
 
     public MainWindow()
     {
         InitializeComponent();
+        InitializeUserInputActions();
         _gameSession.OnMessageRaised += OnGameMessageRaised;
         DataContext = _gameSession;
     }
@@ -53,13 +58,59 @@ public partial class MainWindow : Window
 
     private void OnClick_DisplayTradeScreen(object sender, RoutedEventArgs e)
     {
-        TradeScreen tradeScreen = new() { Owner = this, DataContext = _gameSession };
-        tradeScreen.ShowDialog();
+        if (_gameSession.CurrentTrader != null)
+        {
+            TradeScreen tradeScreen = new() { Owner = this, DataContext = _gameSession };
+            tradeScreen.ShowDialog();
+        }
     }
 
-    public void OnGameMessageRaised(object? sender, GameMessagesEventArgs e)
+    private void OnGameMessageRaised(object? sender, GameMessagesEventArgs e)
     {
         GameMessages.Document.Blocks.Add(new Paragraph(new Run(e.Mesagge)));
         GameMessages.ScrollToEnd();
+    }
+
+    private void OnClick_Craft(object sender, RoutedEventArgs e)
+    {
+        if (((FrameworkElement)sender).DataContext is Recipe recipe)
+        {
+            _gameSession.CraftItem(recipe);
+        }
+    }
+
+    private void InitializeUserInputActions()
+    {
+        _userInputActions.Add(Key.W, () => _gameSession.Move(Direction.North));
+        _userInputActions.Add(Key.A, () => _gameSession.Move(Direction.West));
+        _userInputActions.Add(Key.S, () => _gameSession.Move(Direction.South));
+        _userInputActions.Add(Key.D, () => _gameSession.Move(Direction.East));
+        _userInputActions.Add(Key.Z, () => _gameSession.AttackCurrentMonster());
+        _userInputActions.Add(Key.C, () => _gameSession.UseCurrentConsumable());
+        _userInputActions.Add(Key.I, () => SetTabFocus("InventoryTabItem"));
+        _userInputActions.Add(Key.Q, () => SetTabFocus("QuestsTabItem"));
+        _userInputActions.Add(Key.R, () => SetTabFocus("RecipesTabItem"));
+        _userInputActions.Add(Key.T, () => OnClick_DisplayTradeScreen(this, new RoutedEventArgs()));
+    }
+
+    private void MainWindow_OnKeyDown(object sender, KeyEventArgs e)
+    {
+        if (_userInputActions.ContainsKey(e.Key))
+            _userInputActions[e.Key].Invoke();
+    }
+
+    private void SetTabFocus(string tabName)
+    {
+        foreach (var item in PlayerDataTabControl.Items)
+        {
+            if (item is TabItem tabItem)
+            {
+                if (tabItem.Name == tabName)
+                {
+                    tabItem.IsSelected = true;
+                    return;
+                }
+            }
+        }
     }
 }
